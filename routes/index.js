25bd24403c61fe5de53817b13f182b60208a1746
Natalie Macias <nramirez787@gmail.com>
2016-04-30 15:31:15 -1000

    WIP use express router
    requires counter routes
    is not working
    
    references #3

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const counter = require('./counter');
+router.use('/counter', counter);
+
+module.exports = router;
